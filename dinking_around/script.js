82507bb9bf3cd6047a1030ad9ac12aa4670dbaf6
Ty Burningham <tyburningham@mail.weber.edu>
2026-01-29 11:29:33 -0700

    Starting to play with js

---
+++ dinking_around/script.js
@@ -0,0 +1,11 @@
+alert("Welcome to my page! Enjoy your stay.");
+
+// Both double and single quotes work in JavaScript.
+let num1 = 5;
+let num2 = 4.6;
+let age = 30, name = "John", flag = true;
+//You can declare multiple variables in one line.
+
+alert('Hello ' + name + ', you are ' + age + ' years old.');
+
+console.log("The sum of num1 and num2 is: " + (num1 + num2));
