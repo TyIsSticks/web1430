0a30a72ae2b32cb6226b55a98f61894aecdab2ea
Ty Burningham <tyburningham@mail.weber.edu>
2026-02-10 12:15:11 -0700

    75% done with module 2 assignment

---
+++ JavaScriptProfile/script.js
@@ -0,0 +1,14 @@
+let firstName = prompt("What is your first name?");
+let lastName = prompt("What is your last name?");
+let nickname = prompt("What's a funny nickname for you?");
+console.log("Hello! My name is " + firstName + " " + lastName + ", but you can call me " + nickname + ".");
+console.log("Your full name has " + (firstName.length + lastName.length) + " characters in it.");
+let height = prompt("How tall are you in centimeters?");
+let weight = prompt("How much do you weigh in kilograms?");
+console.log("Your height in inches is " + (height / 2.54).toFixed(2) + " inches.");
+console.log("Your weight in pounds is " + (weight * 2.20462).toFixed(2) + " pounds.");
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log("Fun Fact: Did you know " + randomNumber + " is a cool number about me?");
+let goal = prompt("What is one of your career goals?");
+console.log("One of my career goals is to " + goal + ".");
+console.log("This goal has " + goal.length + " characters in it.");
