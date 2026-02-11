71cdda6b0c6b9e459ef306091078aa33ddc430f4
Ty Burningham <tyburningham@mail.weber.edu>
2026-02-11 11:31:02 -0700

    Finished assignment. Added more console logs.

--- JavaScriptProfile/script.js
+++ JavaScriptProfile/script.js
@@ -12,3 +12,9 @@ console.log("Fun Fact: Did you know " + randomNumber + " is a cool number about 
 let goal = prompt("What is one of your career goals?");
 console.log("One of my career goals is to " + goal + ".");
 console.log("This goal has " + goal.length + " characters in it.");
+let birthYear = prompt("What year were you born?");
+let currentYear = new Date().getFullYear();
+let age = currentYear - birthYear;
+console.log("You are " + age + " years old. You have lived approximately " + (age * 365) + " days on Earth!");
+alert("Today's date is " + new Date().toLocaleDateString() + ". Thanks for sharing about yourself!"); //Display date as mm/dd/yyyy format
+console.log("Today's date is " + new Date().toLocaleDateString() + "There are " + (365 - new Date().getDay()) + " days left in the year!"); //Display date and days left in the year
